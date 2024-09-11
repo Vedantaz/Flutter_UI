6f957508841b47d5a5d223284dc0e8908ce613ca
Vedantaz <muleyvedant4@gmail.com>
2024-09-11 18:30:37 +0530

    added shadow effects, full-sized widths, almost all is done, just itesm numbers are left

--- lib/main.dart
+++ lib/main.dart
@@ -59,20 +59,22 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  color: const Color.fromARGB(255, 186, 10, 195),
-                  height: 100,
-                  width: 1370,
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'First SilverAppBar',
-                        style: TextStyle(color: Colors.white, fontSize: 12),
-                      )
-                    ],
+                Expanded(
+                  // Use Expanded for 100% width
+                  child: Container(
+                    color: const Color.fromARGB(255, 186, 10, 195),
+                    height: 100,
+                    child: const Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'First SilverAppBar',
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        )
+                      ],
+                    ),
                   ),
-                )
+                ),
               ],
             ),
             Padding(
@@ -80,117 +82,228 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Container(
-                    // decoration: BoxDecoration(
-                    //   borderRadius: BorderRadius.circular(10),
-                    //   // Adjust the radius as needed
-                    // ),
-                    color: const Color.fromARGB(255, 4, 60, 200),
-                    width: 675,
-                    height: 100,
+                  const SizedBox(width: 5),
+                  Expanded(
+                    flex: 10, // Adjust flex value as needed
+                    child: Container(
+                      color: const Color.fromARGB(255, 4, 60, 200),
+                      height: 100,
+                    ),
                   ),
-                  Container(
-                    // decoration: BoxDecoration(
-                    //   borderRadius: BorderRadius.circular(10),
-                    //   // Adjust the radius as needed
-                    // ),
-                    color: const Color.fromARGB(255, 123, 158, 245),
-                    width: 675,
-                    height: 100,
+                  const SizedBox(width: 20),
+                  Expanded(
+                    flex: 10, // Adjust flex value as needed
+                    child: Container(
+                      color: const Color.fromARGB(255, 123, 158, 245),
+                      height: 100,
+                    ),
                   ),
+                  const SizedBox(width: 5),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0), // Top margin
-              child: Container(
-                // Container with yellow background
-                color: Colors.yellow,
-                height: 200.0, // Set desired height
-                width: 2000.0, // Set desired width
-                child: Column(
-                  // Inner Column
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      // Padding for image with bottom margin
-                      padding: const EdgeInsets.only(bottom: 20.0),
-                      child:
-                          Image.asset('../logo.jpg', height: 100, width: 100),
-                    ),
-                    const Text(
-                      'Second SilverAppBar',
-                      style: TextStyle(color: Colors.white, fontSize: 30),
-                    )
-                  ],
-                ),
-              ),
-            ),
+            // Padding(
+            //   padding: const EdgeInsets.only(top: 10.0),
+            //   child: Container(
+            //     color: Colors.yellow,
+            //     height: 200.0,
+            //     width: 2000.0,
+            //     child: Column(
+            //       // Inner Column
+            //       mainAxisAlignment: MainAxisAlignment.center,
+            //       children: [
+            //         Padding(
+            //           // Padding for image with bottom margin
+            //           padding: const EdgeInsets.only(bottom: 20.0),
+            //           child: ClipRRect(
+            //             borderRadius: BorderRadius.circular(50.0),
+            //             child:
+            //                 Image.asset('../logo.jpg', height: 100, width: 100),
+            //           ),
+            //         ),
+            //         const Text(
+            //           'Second SilverAppBar',
+            //           style: TextStyle(color: Colors.white, fontSize: 30),
+            //         )
+            //       ],
+            //     ),
+            //   ),
+            // ),
+
+            // Padding(
+            //   padding: const EdgeInsets.only(top: 10.0),
+            //   child: Container(
+            //     color: Colors.yellow,
+            //     height: 200.0,
+            //     child: Expanded(
+            //       child: Stack(
+            //         children: [
+            //           Positioned(
+            //             bottom: 0,
+            //             left: 0,
+            //             right: 0,
+            //             child: Center(
+            //               child: Column(
+            //                 mainAxisAlignment: MainAxisAlignment.end,
+            //                 children: [
+            //                   Padding(
+            //                     padding: const EdgeInsets.only(bottom: 10.0),
+            //                     child: ClipRRect(
+            //                       borderRadius: BorderRadius.circular(50.0),
+            //                       child: Image.asset('../logo.jpg',
+            //                           height: 100, width: 100),
+            //                     ),
+            //                   ),
+            //                   const Padding(
+            //                     padding: EdgeInsets.only(
+            //                         bottom:
+            //                             10.0), // Add bottom padding for text
+            //                     child: Text(
+            //                       'Second SilverAppBar',
+            //                       style: TextStyle(
+            //                           color: Colors.white, fontSize: 24),
+            //                     ),
+            //                   )
+            //                 ],
+            //               ),
+            //             ),
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //   ),
+            // ),
+
             Padding(
               padding: const EdgeInsets.only(top: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              child: Column(
+                // Use Column for vertical layout
                 children: [
-                  Container(
-                    color: const Color.fromARGB(255, 254, 141, 12),
-                    width: 200,
-                    height: 50,
-                  ),
+                  // First Container with image and text (modified from previous code)
                   Container(
                     color: Colors.yellow,
-                    width: 200,
-                    height: 50,
+                    height: 200.0,
+                    child: Expanded(
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: 10,
+                            left: 0,
+                            right: 0,
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Padding(
+                                    padding:
+                                        const EdgeInsets.only(bottom: 10.0),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(50.0),
+                                      child: Image.asset('../logo.jpg',
+                                          height: 100, width: 100),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  const Padding(
+                                    padding: EdgeInsets.only(bottom: 10.0),
+                                    child: Text(
+                                      'Second SilverAppBar',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 24),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  Container(
-                    color: Colors.yellow,
-                    width: 200,
-                    height: 50,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 6.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    color: const Color.fromARGB(255, 0, 255, 183),
-                    width: 200,
-                    height: 50,
-                  ),
-                  Container(
-                    color: const Color.fromARGB(255, 0, 255, 183),
-                    width: 200,
-                    height: 50,
-                  ),
-                  Container(
-                    color: const Color.fromARGB(255, 0, 255, 183),
-                    width: 200,
-                    height: 50,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 6.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Container(
-                    color: const Color.fromARGB(255, 255, 59, 59),
-                    width: 200,
-                    height: 50,
-                  ),
-                  Container(
-                    color: const Color.fromARGB(255, 255, 59, 59),
-                    width: 200,
-                    height: 50,
-                  ),
-                  Container(
-                    color: const Color.fromARGB(255, 255, 59, 59),
-                    width: 200,
-                    height: 50,
+
+                  // Row for the three containers with spacing
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      // Container 1
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 231, 178, 3),
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 2,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          width: double
+                              .infinity, // Ensures full width within its column
+                          height: 50,
+                          child: const Center(
+                            child: Text(
+                              'Item 0',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 20),
+                      // Container 2 (similar structure)
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 2,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          width: double.infinity,
+                          height: 50,
+                          child: const Center(
+                            child: Text(
+                              'Item 1',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 219, 81, 1),
+                            borderRadius: BorderRadius.circular(10.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 2,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                          ),
+                          width: double.infinity,
+                          height: 50,
+                          child: const Center(
+                            child: Text(
+                              'Item 2',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
